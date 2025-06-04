df0b9ca52df02b69cebd2f1b13f121811ea60153
Rajshree <101933712+itzrajshree@users.noreply.github.com>
2025-06-04 12:23:52 +0530

    Initial setup using EXPO

---
+++ locals-for-tourists/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My first ever app on expo</Text>
+      <Text>This is the basic screen that opens up first on the app and the webpage.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
